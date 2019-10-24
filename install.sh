f3a99d894ffdd3cfb4b515cf6b9231b0e8367fb6
gazelle0130 <takahiro.hoshi0130@gmail.com>
2019-10-24 15:39:23 +0900

    fix comments

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# dir
+# Create required directories
 mkdir -p ~/.config/nvim
 mkdir -p ~/.ctags.d
 
-# create symlinks
+# Create symlinks
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
