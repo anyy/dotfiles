b5c4c68c39b8747c0010a509fd61e765d9860e6f
gazelle0130 <takahiro.hoshi0130@gmail.com>
2019-11-05 18:35:49 +0900

    add a symlink

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.ctags.d
 
 # Create symlinks
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tigrc ~/.tigrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.config/nvim/dein.toml ~/.config/nvim/dein.toml
