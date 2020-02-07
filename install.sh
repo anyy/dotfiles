61951fc2fba67c1ec3f526b58175c079868a09d1
anyy <takahiro.hoshi0130@gmail.com>
2020-02-07 21:10:28 +0900

    add .tmux.conf

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ ln -s ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -s ~/dotfiles/.ctags.d/go.ctags ~/.ctags.d/go.ctags
 ln -s ~/dotfiles/.curlrc ~/.curlrc
 ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # Sets macOS defaults
 source ~/dotfiles/.macos
